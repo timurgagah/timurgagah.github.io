54c9c7226ca80abae42785d474599f8111944521
timurgagah <69243923+timurgagah@users.noreply.github.com>
2022-02-08 20:15:46 +0700

    Add files via upload
    Tes Web pertama

---
+++ tes_file.js
@@ -0,0 +1,8 @@
+function fungsiSaya() {
+    let s = document.getElementById("tes");
+    if (s.style.display === "none") {
+        s.style.display = "flex";
+    } else {
+        s.style.display = "none";
+    }
+}
